0f50b3d4657195bb9802625a0b7393e634a11bcc
brsteeper <brsteeper@gmail.com>
2016-12-14 22:50:21 -0800

    Made a new page. /gravRider/ will be a form. /gravRider/visual/ will display input

--- solarSystem/gravRider/models.py
+++ solarSystem/gravRider/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Body(models.Model):
+	name = models.CharField(max_length = 50)
+	mass = models.FloatField()
+
+	def __str__(self):
+		return self.name
--- solarSystem/gravRider/views.py
+++ solarSystem/gravRider/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
+def visualization(request):
 	content = {'stuff':1234}
 	return render(request, 'gravRider/visualization.html', content)
 
-
+def form(request):
+	return render(request, 'gravRider/form.html')
