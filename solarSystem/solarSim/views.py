c9561188b8ec8364a2abac5c81069e7bed73d891
brsteeper <brsteeper@gmail.com>
2016-12-23 23:39:35 -0800

    new branch. Display is getting data from index, but not in the way I imagined it would

--- solarSystem/solarSim/views.py
+++ solarSystem/solarSim/views.py
@@ -3,12 +3,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import CreatePlanetForm
+from .models import Planet
 
 def index (request):
-#	content = {'planet_name':'Jupiter',
-#				'mass':778000000}
-	form = CreatePlanetForm
-	return render(request, 'solarSim/index.html', {'form': form})
+	if request.method == 'POST':
+		form = CreatePlanetForm(request.POST)
+		if form.is_valid():
+			planet = Planet()
+			planet.name = form.cleaned_data["planet_name"]
+			planet.mass = form.cleaned_data["planet_mass"]
+			planet.save()
+			return render(request, 'solarSim/display.html', {'planetID': planet.id}) #I want this to call display through its def in this file not through the .html. I think?
+	else:
+		form = CreatePlanetForm
+		return render(request, 'solarSim/index.html', {'form': form, 'action_path' : request.path})
 
-def display (request):
-	return render(request, 'solarSim/display.html')
+def display (request, planetID):
+	planet = Planet.objects.get(pk=plantID)
+	return render(request, 'solarSim/display.html', {'planet': planet})
